181924f496f108b4ffaea8516a23f4414fe5029d
Jossie Quintero <quinterjosy@gmail.com>
2022-01-01 22:18:21 -0500

    Adding pause/play to the song

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("All right!");
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById("dancin");
+    audio.loop = true;
+    audio.volume = 1;
+
+    const volume = document.getElementById("volume");
+    volume.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+            console.log(this);
+            this.children[0].classList.remove("hidden")
+            this.children[1].classList.add("hidden");
+        } else {
+            audio.pause();
+            this.children[0].classList.add("hidden");
+            this.children[1].classList.remove("hidden");
+        }
+    });
+});
